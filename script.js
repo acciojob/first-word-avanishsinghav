5fa512a1afd0c72d8731cd19029fcaa9180daa4b
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-11 19:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	let temp = s.trim();
+	let str = "";
+	for(let i = 0; i<temp.length; i++){
+		if(temp[i] === " "){
+			return str;
+		}
+		else{
+			str += temp[i];
+		}
+	}
+	return str;
 }
 
 // Do not change the code below
